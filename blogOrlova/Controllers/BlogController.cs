2da3aa4e43fab127fb93cf4df52e5f8053c1995d
Elaine-ButterflY <black.coffee18@yandex.ru>
2020-09-26 13:58:35 +0400

    Добавьте файлы проекта.

---
+++ blogOrlova/Controllers/BlogController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blogOrlova.Controllers
+{
+    /// <summary>
+    /// Контроллер приложения blogOrlova. В нём хранятся методы, отображающие данные блога.
+    /// </summary>
+    public class BlogController : Controller
+    {
+        /// <summary>
+        /// Отображение данных страницы РАСХИТИТЕЛЬНИЦА ГРОБНИЦ
+        /// </summary>
+        /// <returns>Возвращает представление, страницу на HTML-коде, которая содержит информацию о первой игре серии.</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Отображение данных страницы ХРОНИКИ
+        /// </summary>
+        /// <returns>Возвращает представление, страницу на HTML-коде, которая содержит информацию о пятой игре серии.</returns>
+        public IActionResult Index1()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Отображение данных страницы 2013
+        /// </summary>
+        /// <returns>Возвращает представление, страницу на HTML-коде, которая содержит информацию о девятой игре серии.</returns>
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
---
+++ blogOrlova/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using blogOrlova.Models;
+
+namespace blogOrlova.Controllers
+{
+    /// <summary>
+    /// Контроллер приложения blogOrlova. В нём описывается работа с веб-сервером.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="logger">Через него посредством механизма dependency injection передается сервис ILogger, используемый для логгирования.</param>
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// Отображение данных страницы ЛАРА КРОФТ
+        /// </summary>
+        /// <returns>Возвращает представление, главную страницу</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Отображение данных страницы Конфиденциальность
+        /// </summary>
+        /// <returns>Возвращает представление, страницу политики конфиденциальности</returns>
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
